11ac066dcb770ccb2e57b575f8bb3ea1c19a8c42
Pengo Wray <peter@becauseofgames.com>
2024-11-13 17:40:53 +1100

    Build for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,21 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+/* ---added--- */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repo = 'knights-graph';
+
+module.exports = {
+  output: 'export',
+  distDir: 'docs',
+  assetPrefix: isGithubActions ? `/${repo}/` : '',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubActions ? `/${repo}` : '',
+};
+/* ---end of added--- */
+
+
+
 export default nextConfig;
